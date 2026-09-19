d470e591d4ce6f35c99c72e5a09d3888f7220428
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-09-19 19:56:42 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -39,23 +39,23 @@ const updateStatus = () => {
 createRoot(document.querySelector("body")!).render(
   <StrictMode>
 
-    <main className="flex justify-center items-center flex-col min-h-dvh gap-5" style={{backgroundColor: updateStatus().bg}}>
+    <main className="flex justify-center items-center flex-col min-h-dvh gap-5 font-sans" style={{backgroundColor: updateStatus().bg}}>
 
       <header className="text-center">
         
         <span className="text-[40px]">⏱️</span>
         
-        <h1 className="font-extrabold">Dynamic Hour</h1>
+        <h1 className="font-extrabold text-3xl">Dynamic Hour</h1>
       
       </header>
 
       <article className="bg-white rounded-lg p-4 gap-3 flex justify-center items-center flex-col w-full max-w-87.5">
 
-        <h2 className="font-bold">Hi, it's {hours} o'clock now</h2>
+        <h2 className="font-bold text-[16px]">Hi, it's {hours} o'clock now</h2>
 
         <img className="w-full max-w-50 h-50 rounded-[100%]" src={updateStatus().img} />
 
-        <h3 className="bg-[whitesmoke] p-1.5 rounded-xi">{updateStatus().status}</h3>
+        <h3 className="bg-[#b9b9b9] p-1.5 rounded-xl">{updateStatus().status}</h3>
 
         <p>{updateStatus().obs}</p>
 
@@ -65,7 +65,7 @@ createRoot(document.querySelector("body")!).render(
         
         <img className="w-7.5" src={IconGithub} />
         
-        <h6>Project developed by: <a className="font-bold" target="_blank" href="https://github.com/GuilhermeSoaresMarciel">Guilherme Soares Marciel</a></h6>
+        <h6 className="text-[12px]">Project developed by: <a className="font-bold" target="_blank" href="https://github.com/GuilhermeSoaresMarciel">Guilherme Soares Marciel</a></h6>
       
       </footer>
 
